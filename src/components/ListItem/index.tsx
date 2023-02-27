9d98e126eba436780a69e1d0a7ef2284a6bec058
sergiocsouza <sergiodconceicaosouza@gmail.com>
2023-02-27 09:18:00 -0300

    selecionar texto e marcar check-box

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -3,31 +3,32 @@ import { Item } from '../../types/item';
 import { useState } from 'react';
 
 type Props = {
-    item: Item,
-    onChange: (id: number, done: boolean) => void,
-    onRemove: (id: number, done: boolean) => void;
+  item: Item,
+  onChange: (id: number, done: boolean) => void,
+  onRemove: (id: number, done: boolean) => void;
 }
 
 export const ListItem = ({ item, onChange, onRemove }: Props) => {
-    const [isChecked, setIsChecked] = useState(item.done);
+  const [isChecked, setIsChecked] = useState(item.done);
 
-    const handleRemove = () => {
-        onRemove(item.id, isChecked);
-      }
+  const handleRemove = () => {
+    onRemove(item.id, isChecked);
+  }
 
+  const inputId = `item-${item.id}`; // gera um id único para o input
 
-
-    return (
-        <C.Container done={item.done}>
-            
-
-            <input
-                type="checkbox"
-                checked={item.done}
-                onChange={e => onChange(item.id, e.target.checked)}
-            />
-            <label >{item.name}</label>
-            <button onClick={handleRemove} >Remover</button>
-   </C.Container>
-    );
+  return (
+    <C.Container done={item.done}>
+      
+        <input
+          id={inputId} // define o id do input
+          type="checkbox"
+          checked={item.done}
+          onChange={e => onChange(item.id, e.target.checked)}
+        />
+        <label htmlFor={inputId}>{item.name}</label>
+   
+      <button onClick={handleRemove}>Remover</button>
+    </C.Container>
+  );
 }
